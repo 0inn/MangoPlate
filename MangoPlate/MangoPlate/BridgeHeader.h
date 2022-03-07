f69d0846ca5494ea69f4dc4fbece4c944e73a446
0inn <bueaty1220@naver.com>
2022-03-07 14:23:49 +0900

    init

---
+++ MangoPlate/MangoPlate/BridgeHeader.h
@@ -0,0 +1,13 @@
+//
+//  BridgeHeader.h
+//  KaKaoMapTest
+//
+//  Created by 김영인 on 2022/02/25.
+//
+
+#ifndef BridgeHeader_h
+#define BridgeHeader_h
+
+#import <DaumMap/MTMapView.h>
+
+#endif /* BridgeHeader_h */
